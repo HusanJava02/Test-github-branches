de2e6e560120d69d8895e81d2b4273426726d6c8
Husanboy <husanboysobirjonov02@gmail.com>
2022-01-29 16:00:27 +0500

    functions added

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 printf('salom')
+def salom():
+	pass
+	adn
+
+ class classofclassend(object):
+ 	"""docstring for classofclassend"""
+ 	def __init__(self, arg):
+ 		super(classofclassend, self).__init__()
+ 		self.arg = arg
+ 		alkda
+ 	
+ 		
